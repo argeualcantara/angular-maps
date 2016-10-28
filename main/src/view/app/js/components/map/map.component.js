acf07b48fd552e6884dcd3b71a81043d07787058
Argeu <argeu.alcantara@hp.com>
2016-10-28 16:54:41 -0300

    Showing my first route

--- main/src/view/app/js/components/map/map.component.js
+++ main/src/view/app/js/components/map/map.component.js
@@ -3,12 +3,18 @@ import './map.scss';
 class MapController {
   constructor(mapService) {
     this.mapService = mapService;
-    this.mapService.map = L.map('mapid').setView([-3.757776,-38.4885972], 17);
+    this.mapService.map = L.map('mapid').setView(this.mapService.currentPath[0], 17);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/argeualcantara/ciuslx5ql00vq2inoc0eemgod/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJnZXVhbGNhbnRhcmEiLCJhIjoiY2l1c2x2b2g2MDBiZzJ0cnY1NDB3MmtvOSJ9.wm7SbtgDlapj2U_Z42byTg', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 20
     }).addTo(this.mapService.map);
+
+    let markerStart = L.marker(this.mapService.currentPath[0]).addTo(this.mapService.map);
+    let markerEnd = L.marker(this.mapService.currentPath[this.mapService.currentPath.length-1]).addTo(this.mapService.map);
+    this.mapService.currentMarkers.push(markerStart);
+    this.mapService.currentMarkers.push(markerEnd);
+    var polyline = L.polyline(this.mapService.currentPath).addTo(this.mapService.map);
 
     this.mapService.map.on('click', (e) => {
       let marker = L.marker(e.latlng).addTo(this.mapService.map);
